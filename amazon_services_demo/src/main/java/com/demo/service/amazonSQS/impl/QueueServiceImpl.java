928c9966639897812489a9025ea1efa57a9b7b99
yaroslav <slavkosoltys@ukr.net>
2020-01-23 17:01:30 +0200

    added kafka added micrometer endpoint config

--- amazon_services_demo/src/main/java/com/demo/service/amazonSQS/impl/QueueServiceImpl.java
+++ amazon_services_demo/src/main/java/com/demo/service/amazonSQS/impl/QueueServiceImpl.java
@@ -2,7 +2,9 @@ package com.demo.service.amazonSQS.impl;
 
 import com.demo.service.amazonSQS.QueueService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueueServiceImpl implements QueueService {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @SqsListener("my_from_sns")
     public void messageHandler(String message, @Header("SenderId") String senderId) {
         log.info("received message from sender id:{}\nMessage:\n{}", senderId, message);
-
+        kafkaTemplate.send("from_sqs", message);
     }
 
 }
